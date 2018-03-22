418aa44919960c384ecdcd29dc9a90326ba9444a
Vladymir González <kuez.kuez@gmail.com>
2018-03-22 13:35:04 -0600

    Update config.py

--- optional-kubernetes-engine/config.py
+++ optional-kubernetes-engine/config.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License. gola
 
 """
 This file contains all of the configuration values for the application.
